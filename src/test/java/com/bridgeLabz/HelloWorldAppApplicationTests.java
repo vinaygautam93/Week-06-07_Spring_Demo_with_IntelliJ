ba98232cfb7e6b6f307c7f5d15e7f714d6eec5b3
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 08:42:12 +0530

    [Vinay] Added HelloWorldAppApplicationTests

---
+++ src/test/java/com/bridgeLabz/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
