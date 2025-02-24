e91289d4b33242fb2a1ff0f925e3f9695589720d
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 08:46:18 +0530

    [Vinay] Implemented Hello World Console Output

---
+++ src/main/java/com/bridgeLabz/HelloWorldAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+}
